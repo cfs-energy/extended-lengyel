WIP fixed impurities Need to fix Zeff calculation

extended_lengyel/extended_lengyel_model/Lengyel_model_core.py
@@ -1,12 +1,14 @@
 """Core functions for evaluating the Lengyel model."""
 
 import xarray as xr
+import numpy as np
 from cfspopcon import Algorithm
 from cfspopcon.formulas.impurities.edge_radiator_conc import build_L_int_integrator as _build_L_int_integrator
 from cfspopcon.named_options import AtomicSpecies
 from cfspopcon.formulas.atomic_data import AtomicData
 from cfspopcon.unit_handling import convert_units, magnitude, ureg, wraps_ufunc, Unitfull
 from scipy.interpolate import InterpolatedUnivariateSpline  # type:ignore[import-untyped]
+from typing import Self
 
 
 def item(val):
@@ -18,7 +20,7 @@ def item(val):
 
 
 class L_int_integrator:
-    """Class to hold a mixed-seeding L-int integrator."""
+    """Class to hold an L-int integrator."""
 
     def __init__(
         self,
@@ -44,7 +46,7 @@ class L_int_integrator:
 
     def __call__(self, start_temp: Unitfull, stop_temp: Unitfull) -> Unitfull:
         """Return sum_z(Lint(start_temp, stop_temp) * wz), handling input and output units."""
-        weighted_L_INT = 0.0
+        weighted_L_INT = 0.0 * ureg.W * ureg.m**3 * ureg.eV**1.5
 
         for weight, integrator in zip(self.weights_list, self.integrators):
             weighted_L_INT += weight * integrator(start_temp, stop_temp)
@@ -59,6 +61,11 @@ class L_int_integrator:
             weighted_L_INT += weight * integrator.unitless_func(start_temp, stop_temp)
 
         return weighted_L_INT
+    
+    @classmethod
+    def empty(cls) -> Self:
+        """Returns an empty L_int_integrator which always returns 0.0."""
+        return cls(impurity_species_list=[], impurity_weights_list=[], atomic_data=None)
 
 
 @Algorithm.register_algorithm(return_keys=["L_int_integrator"])
@@ -118,8 +125,8 @@ def _build_mean_charge_interpolator(
 
 
 
-class Mean_charge_interpolator:
-    """Class to hold a mixed-seeding mean charge interpolator."""
+class Change_in_Zeff_interpolator:
+    """Class to hold a mixed-seeding Zeff interpolator."""
 
     def __init__(
         self,
@@ -129,7 +136,7 @@ class Mean_charge_interpolator:
         reference_ne_tau: Unitfull = 0.5 * ureg.ms * ureg.n20,
         reference_electron_density: Unitfull = 1.0 * ureg.n20,
     ):
-        """Initializes a Mean_charge_interpolator from linked lists of impurity species and weights."""
+        """Initializes a Change_in_Zeff_interpolator from linked lists of impurity species and weights."""
         self.interpolators = []
         self.weights_list = impurity_weights_list
 
@@ -144,7 +151,7 @@ class Mean_charge_interpolator:
             self.interpolators.append(interpolator)
 
     def __call__(self, electron_temp: Unitfull) -> Unitfull:
-        """Return sum_z(<Z>(electron_temp) * wz) / sum_z(wz), handling input and output units."""
+        """Return the change in Zeff due to each impurity species, handling input and output units."""
         weighted_mean_charge = 0.0
         total_weights = 0.0
 
@@ -152,7 +159,7 @@ class Mean_charge_interpolator:
             weighted_mean_charge += weight * interpolator(electron_temp)
             total_weights += weight
 
-        return weighted_mean_charge / total_weights
+        return weighted_mean_charge / np.maximum(total_weights, np.finfo(float).eps)
 
     def unitless_eval(self, electron_temp: Unitfull) -> Unitfull:
         """Return sum_z(<Z>(electron_temp) * wz) / sum_z(wz), without handling input and output units."""
@@ -163,7 +170,12 @@ class Mean_charge_interpolator:
             weighted_mean_charge += weight * interpolator.unitless_func(electron_temp)
             total_weights += weight
 
-        return weighted_mean_charge / total_weights
+        return weighted_mean_charge / np.maximum(total_weights, np.finfo(float).eps)
+    
+    @classmethod
+    def empty(cls) -> Self:
+        """Returns an empty Change_in_Zeff_interpolator which always returns 0.0."""
+        return cls(impurity_species_list=[], impurity_weights_list=[], atomic_data=None)
 
 
 @Algorithm.register_algorithm(return_keys=["mean_charge_interpolator"])
@@ -174,8 +186,8 @@ def build_mean_charge_interpolator(
     reference_ne_tau=0.5 * ureg.ms * ureg.n20,
     reference_electron_density=1.0 * ureg.n20,
 ):
-    """Build a mean charge state interpolator which returns sum(w_z <Z>)/sum(w_z)."""
-    return Mean_charge_interpolator(
+    """Build a change-in-Zeff interpolator which returns how much each species changes Z-effective."""
+    return Change_in_Zeff_interpolator(
         impurity_species_list=impurity_species_list,
         impurity_weights_list=impurity_weights_list,
         atomic_data=atomic_data,

extended_lengyel/extended_lengyel_model/Lengyel_model_extended_S.py
@@ -3,11 +3,12 @@
 import numpy as np
 import xarray as xr
 from cfspopcon import Algorithm, CompositeAlgorithm
+from cfspopcon.unit_handling import ureg
 
-from .Lengyel_model_core import item
+from .Lengyel_model_core import item, L_int_integrator
 
 
-@Algorithm.register_algorithm(return_keys=["impurity_fraction", "radiated_fraction_above_xpt", "radiated_fraction_below_xpt"])
+@Algorithm.register_algorithm(return_keys=["impurity_fraction", "radiated_fraction_above_xpt"])
 def run_extended_lengyel_model_with_S_correction(
     q_parallel,
     divertor_broadening_factor,
@@ -19,37 +20,41 @@ def run_extended_lengyel_model_with_S_correction(
     electron_temp_at_cc_interface,
     divertor_entrance_electron_temp,
     L_int_integrator,
+    background_cz_L_int = L_int_integrator.empty(),
     mask_invalid_results: bool = True,
 ):
     """Calculate the impurity fraction required to radiate a given fraction of the power in the scrape-off-layer."""
-    LINT_cc_div = item(L_int_integrator)(electron_temp_at_cc_interface, divertor_entrance_electron_temp)
-    LINT_div_u = item(L_int_integrator)(divertor_entrance_electron_temp, separatrix_electron_temp)
-    LINT_cc_u = item(L_int_integrator)(electron_temp_at_cc_interface, separatrix_electron_temp)
+    # Seed impurities
+    Ls_cc_div = item(L_int_integrator)(electron_temp_at_cc_interface, divertor_entrance_electron_temp)
+    Ls_div_u = item(L_int_integrator)(divertor_entrance_electron_temp, separatrix_electron_temp)
+    Ls_cc_u = item(L_int_integrator)(electron_temp_at_cc_interface, separatrix_electron_temp)
+    
+    # Fixed impurities
+    Lf_cc_div = item(background_cz_L_int)(electron_temp_at_cc_interface, divertor_entrance_electron_temp)
+    Lf_div_u = item(background_cz_L_int)(divertor_entrance_electron_temp, separatrix_electron_temp)
+    Lf_cc_u = item(background_cz_L_int)(electron_temp_at_cc_interface, separatrix_electron_temp)
 
-    radiated_fraction_above_xpt = 1.0 - np.sqrt(
-        (LINT_cc_div + LINT_div_u * (parallel_heat_flux_at_cc_interface / q_parallel) ** 2)
-        / (LINT_cc_div + LINT_div_u / divertor_broadening_factor**2)
+    qu = q_parallel
+    qcc = parallel_heat_flux_at_cc_interface
+    b = divertor_broadening_factor
+    k = 2.0 * (kappa_e0 / kappa_z) * separatrix_electron_density**2 * separatrix_electron_temp**2
+
+    q_div_squared = (
+        (Ls_div_u * (qcc**2 + k * Lf_cc_div) + Ls_cc_div * (qu**2 - k * Lf_div_u))
+        / (Ls_div_u / b**2  + Ls_cc_div)
     )
-
-    total_radiated_fraction = (1.0 - parallel_heat_flux_at_cc_interface / q_parallel) * divertor_broadening_factor
-    radiated_fraction_above_xpt = np.minimum(radiated_fraction_above_xpt, total_radiated_fraction)
-    radiated_fraction_above_xpt = np.maximum(radiated_fraction_above_xpt, 0.0)
-
-    qdiv = (1 - radiated_fraction_above_xpt) * q_parallel
-    radiated_fraction_below_xpt = 1.0 - parallel_heat_flux_at_cc_interface / qdiv
-
-    kappa = kappa_e0 / kappa_z
+    q_div_squared = np.maximum(q_div_squared, 0.0 * ureg.W**2 * ureg.m**-4)
+    f_rad_main = 1.0 - np.sqrt(q_div_squared) / qu
 
     c_z = (
-        q_parallel**2
-        + ((1 / divertor_broadening_factor) ** 2 - 1) * (1 - radiated_fraction_above_xpt) ** 2 * q_parallel**2
-        - parallel_heat_flux_at_cc_interface**2
-    ) / (2.0 * kappa * separatrix_electron_density**2 * separatrix_electron_temp**2 * LINT_cc_u)
+        (qu**2 + (1 / b**2 - 1) * q_div_squared - qcc**2) / (k * Ls_cc_u)
+        - Lf_cc_u / Ls_cc_u
+    )
 
     if mask_invalid_results:
         c_z = xr.where(c_z < 0.0, np.nan, c_z)
 
-    return c_z, radiated_fraction_above_xpt, radiated_fraction_below_xpt
+    return c_z, f_rad_main
 
 
 CompositeAlgorithm(

extended_lengyel/extended_lengyel_model/Lengyel_model_extended_S_Zeff_alphat.py
@@ -3,7 +3,7 @@
 import numpy as np
 import xarray as xr
 from cfspopcon import Algorithm, CompositeAlgorithm
-
+from typing import Optional
 
 from cfspopcon.unit_handling import ureg
 from cfspopcon.formulas.metrics import calc_alpha_t
@@ -12,14 +12,13 @@ from cfspopcon.formulas.separatrix_conditions.separatrix_operational_space.share
 from cfspopcon.formulas.metrics.larmor_radius import calc_larmor_radius
 
 from .Lengyel_model_extended_S_Zeff import run_extended_lengyel_model_with_S_and_Zeff_correction
-from .Lengyel_model_core import item
+from .Lengyel_model_core import item, L_int_integrator, Mean_charge_interpolator
 
 
 @Algorithm.register_algorithm(
     return_keys=[
         "impurity_fraction",
         "radiated_fraction_above_xpt",
-        "radiated_fraction_below_xpt",
         "z_effective",
         "divertor_entrance_electron_temp",
         "separatrix_electron_temp",
@@ -54,6 +53,8 @@ def run_extended_lengyel_model_with_S_Zeff_and_alphat_correction(
     cylindrical_safety_factor,
     separatrix_average_poloidal_field,
     ratio_of_upstream_to_average_poloidal_field,
+    background_cz_L_int= L_int_integrator.empty(),
+    background_cz_mean_charge= Mean_charge_interpolator.empty(),
     iterations_for_Lengyel_model: int = 5,
     iterations_for_alphat: int = 5,
     mask_invalid_results: bool = True,
@@ -86,7 +87,6 @@ def run_extended_lengyel_model_with_S_Zeff_and_alphat_correction(
         (
             c_z,
             radiated_fraction_above_xpt,
-            radiated_fraction_below_xpt,
             z_effective,
             divertor_entrance_electron_temp,
             separatrix_electron_temp,
@@ -109,13 +109,16 @@ def run_extended_lengyel_model_with_S_Zeff_and_alphat_correction(
             sheath_heat_transmission_factor=sheath_heat_transmission_factor,
             L_int_integrator=L_int_integrator,
             mean_charge_interpolator=mean_charge_interpolator,
+            background_cz_L_int=background_cz_L_int,
+            background_cz_mean_charge=background_cz_mean_charge,
             iterations_for_Lengyel_model=iterations_for_Lengyel_model,
             mask_invalid_results=False,
         )
 
         # Use the separatrix electron temperature to calculate Z-eff for alpha-t
-        mean_z = item(mean_charge_interpolator)(separatrix_electron_temp)
-        z_effective_upstream = 1.0 + mean_z * (mean_z - 1.0) * c_z
+        seed_mean_z = item(mean_charge_interpolator)(separatrix_electron_temp)
+        fixed_mean_z = item(background_cz_mean_charge)(separatrix_electron_temp)
+        z_effective_upstream = 1.0 + seed_mean_z * (seed_mean_z - 1.0) * c_z + fixed_mean_z * (fixed_mean_z - 1.0)
         z_effective_upstream = np.maximum(z_effective_upstream, 1.0)
 
         alpha_t = calc_alpha_t(
@@ -138,7 +141,6 @@ def run_extended_lengyel_model_with_S_Zeff_and_alphat_correction(
     return (
         c_z,
         radiated_fraction_above_xpt,
-        radiated_fraction_below_xpt,
         z_effective,
         divertor_entrance_electron_temp,
         separatrix_electron_temp,