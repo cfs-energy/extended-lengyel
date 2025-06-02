73cf0330420416082f7cee040ffca67e5be4f22a
Tom Body <tbody@cfs.energy>
2025-06-02 09:20:54 -0400

    Use deuterium for testing

--- tests/test_extended_lengyel_model.py
+++ tests/test_extended_lengyel_model.py
@@ -119,16 +119,16 @@ def test_calc_z_effective():
 
     assert np.allclose(magnitude_in_units(z_eff_no_impurities, ureg.dimensionless), 1.0)
 
-    z_eff_hydrogen = calc_z_effective(
+    z_eff_deuterium = calc_z_effective(
         electron_temp=xr.DataArray(np.array([100.0, 120.0]) * ureg.eV, dims="dim_Te"),
         c_z = xr.DataArray(np.array([1.0, 2.0]) * ureg.percent, dims="dim_c_z"),
         mean_charge_for_seed_impurities = Mean_charge_interpolator.empty(),
-        mean_charge_for_fixed_impurities = Mean_charge_interpolator(["Hydrogen"], atomic_data),
+        mean_charge_for_fixed_impurities = Mean_charge_interpolator(["Deuterium"], atomic_data),
         CzLINT_for_seed_impurities = CzLINT_integrator.empty(),
-        CzLINT_for_fixed_impurities = CzLINT_integrator(["Hydrogen"], [1.0], atomic_data),
+        CzLINT_for_fixed_impurities = CzLINT_integrator(["Deuterium"], [1.0], atomic_data),
     )
 
-    assert np.allclose(magnitude_in_units(z_eff_hydrogen, ureg.dimensionless), 1.0, rtol=1e-3)
+    assert np.allclose(magnitude_in_units(z_eff_deuterium, ureg.dimensionless), 1.0, rtol=1e-3)
     
     coronal_ne_tau = 1.0e20 * ureg.m**-3 * ureg.s
 
@@ -140,15 +140,15 @@ def test_calc_z_effective():
     
     assert np.isclose(magnitude_in_units(mean_charge_N(10.0 * ureg.keV), ureg.dimensionless), 7, rtol=1e-3)
 
-    z_eff_hydrogen = calc_z_effective(
+    z_eff_seeding = calc_z_effective(
         electron_temp=xr.DataArray(np.array([10.0, 12.0]) * ureg.keV, dims="dim_Te"),
         c_z = xr.DataArray(np.array([1.0]) * ureg.percent, dims="dim_c_z"),
         mean_charge_for_seed_impurities = mean_charge_N,
-        mean_charge_for_fixed_impurities = Mean_charge_interpolator(["Hydrogen"], atomic_data),
+        mean_charge_for_fixed_impurities = Mean_charge_interpolator(["Deuterium"], atomic_data),
         CzLINT_for_seed_impurities = CzLINT_integrator(["Nitrogen"], [1.0], atomic_data, ne_tau=coronal_ne_tau, rtol_nearest=1),
-        CzLINT_for_fixed_impurities = CzLINT_integrator(["Hydrogen"], [1.0], atomic_data),
+        CzLINT_for_fixed_impurities = CzLINT_integrator(["Deuterium"], [1.0], atomic_data),
     )
 
     z_eff_expected = 1 + 1e-2 * 7 * (7 - 1)
 
-    assert np.allclose(magnitude_in_units(z_eff_hydrogen, ureg.dimensionless), z_eff_expected, rtol=1e-2)
+    assert np.allclose(magnitude_in_units(z_eff_seeding, ureg.dimensionless), z_eff_expected, rtol=1e-2)
