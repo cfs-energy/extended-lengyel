6b6d1f5581cbf8321ac757e3bd6b614873617d41
Tom Body <tbody@cfs.energy>
2025-06-03 09:36:14 -0400

    Handle lists in config

--- extended_lengyel/config.py
+++ extended_lengyel/config.py
@@ -2,43 +2,40 @@
 
 import yaml
 from cfspopcon.unit_handling import Quantity, UndefinedUnitError, ureg
-from cfspopcon import named_options
-from typing import Any, Optional
+from cfspopcon.named_options import AtomicSpecies
+from fractions import Fraction
+from typing import Any, Optional, Callable
 import xarray as xr
 
 from extended_lengyel.directories import notebook_dir
 
 
-def convert_elements(config):
-    """Recursive conversion for config elements."""
-    for k, v in config.items():
-        if isinstance(v, dict):
-            config[k] = convert_elements(v)
-        else:
-            try:
-                val = Quantity(v)
+def test_convert(element: str, conversion: Callable[[str], Any]) -> Any | None:
+    """Use the conversion routine to convert a string to another type. If this fails, return None."""
+    try:
+        return conversion(element)
+    except (ValueError, UndefinedUnitError):
+        return None
 
-                if val.units == "":
-                    val = val.to(ureg.dimensionless).magnitude
-
-                    if val.size == 1:
-                        config[k] = float(val)
-                    else:
-                        config[k] = val
-
-                else:
-                    config[k] = val
-
-            except UndefinedUnitError:
-                pass
-
-        for enum in [named_options.AtomicSpecies]:
-            try:
-                config[k] = enum[v]
-            except (KeyError, TypeError):
-                pass
-
-    return config
+def convert_elements(element):
+    """Read the elements of the configuration and convert them to their underlying types."""
+    if isinstance(element, dict):
+        return {k: convert_elements(v) for k, v in element.items()}
+    elif isinstance(element, list):
+        return [convert_elements(v) for v in element]
+    elif isinstance(element, (float, int)):
+        return element
+    elif isinstance(element, str):
+        if (val:=test_convert(element, float)) is not None:
+            return val
+        if (val:=test_convert(element, lambda s: float(Fraction(s)))) is not None:
+            return val
+        elif (val:=test_convert(element, Quantity)) is not None:
+            return val
+        elif (val:=test_convert(element, lambda s: AtomicSpecies.__getitem__(str.capitalize(s)))) is not None:
+            return val
+    
+    raise NotImplementedError(f"Cannot handle {element} of type {type(element)}")
 
 
 def read_config(
